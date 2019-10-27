d7ef9139da70f360ceeee408ff5dc32c74cec8da
Elizabeth <esandova@stedwards.edu>
2019-10-26 22:59:46 -0500

    adding code to store picture in DB

--- PetPals/SecondViewController.swift
+++ PetPals/SecondViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 import Firebase
 import FirebaseStorage
+import FirebaseFirestore
 
 class SecondViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
 
     @IBOutlet weak var profileImage: UIImageView!
+    let imagePicker = UIImagePickerController()
     
     @IBAction func changeProfilePicBtn(_ sender: Any) {
         handleSelectProfileImageView()
@@ -20,45 +22,103 @@ class SecondViewController: UIViewController, UINavigationControllerDelegate, UI
     
     
     func handleSelectProfileImageView(){
-        let picker = UIImagePickerController()
-        picker.allowsEditing = true
+        imagePicker.allowsEditing = true
+        imagePicker.sourceType = .photoLibrary
         
-        picker.delegate = self
-        present(picker, animated: true, completion: nil)
-    }
-    
-    func uploadImage(){
-        
-        let storageRef = Storage.storage().reference().child("profileImage.png")
-        
-        let uploadData = profileImage.image!.pngData()
-        
-        storageRef.putData(uploadData!)
+        present(imagePicker, animated: true, completion: nil)
         
     }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        
-        
+    @objc func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            if let editedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+                profileImage.image = editedImage
+            } else if let originalImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+                profileImage.image = originalImage
+
+            }
+            profileImage.contentMode = .scaleAspectFit
+
+        }
         dismiss(animated: true, completion: nil)
+        uploadImage()
     }
+        
     
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+    @objc func imagePickerControllerDidCancel(picker: UIImagePickerController){
         print("canceled picker")
-        dismiss(animated: true, completion: nil)
+        dismiss(animated: true,completion: nil)
+    }
+    
+    @objc func uploadImage(){
+        guard let image = profileImage.image, let data = image.pngData() else{
+            let alert = UIAlertController(title: "Error", message: "Problem getting png data.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                NSLog("The \"OK\" alert occured.")
+            }))
+            self.present(alert, animated: true, completion: nil)
+
+            //presentAlert(title: "Error", message: "Something went wrong")
+            return
+        }
+        
+        let imageName = UUID().uuidString
+        let imageReference = Storage.storage().reference().child(imageName)
+        
+        imageReference.putData(data, metadata: nil) { (metadata, err) in
+            if let err = err {
+                let alert = UIAlertController(title: "Error", message: err.localizedDescription, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action" ), style: .default, handler: { _ in
+                    NSLog("The \"OK\" alert occured.")
+                }))
+                self.present(alert, animated: true, completion: nil)
+                return
+            }
+            imageReference.downloadURL(completion: { (url, err) in
+                if let err = err {
+                    let alert = UIAlertController(title: "Error", message: err.localizedDescription, preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment:  "Default action"), style: .default, handler: { _ in
+                        NSLog("The \"OK\" alert occured.")
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                    return
+                }
+                guard let url = url else {
+                    let alert = UIAlertController(title: "Error", message: "Problem uploading image.", preferredStyle: .alert)
+                    alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                        NSLog("The \"OK\" alert occured.")
+                    }))
+                    self.present(alert, animated: true, completion: nil)
+                    
+                    return
+                }
+                let urlString = url.absoluteString
+                
+                let databaseRefernece = Firestore.firestore().collection("ProfileImages")
+                databaseRefernece.addDocument(data: [
+                    "userID" : USERID,
+                    "imageURL" : urlString
+                ]){ err in
+                    if let err = err {
+                        print("Error adding document: \(err)")
+                    } else {
+                        let alert = UIAlertController(title: "Success", message: "Image uploaded successfully.", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+                            NSLog("The \"OK\" alert occured.")
+                        }))
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+            })
+        }
+        
+
+
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
     }
-    
-    //let profileImageViewUIImageView = {
-      //  let profileImage = UIImageView()
-        //profileImage.image = UIImage(named: //"PetPalsIcon")
-        //profileImage//.translatesAutoresizingMaskIntoConstraints = false
-        //profileImage.addGestureRecognizer(UITapGestureRecognizer(target:self,action: #select ))
-    //}
-    
 
 
 }
